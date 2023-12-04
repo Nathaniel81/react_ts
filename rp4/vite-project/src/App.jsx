0e974724254e5380c6f9f9da5ff56724cf6a6a74
Nathaniel81 <nathanield356@gmail.com>
2023-12-04 12:28:26 +0300

    nc

---
+++ rp4/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+
+      <h1 className="text-3xl text-center p-5 bg-slate-700 text-slate-200 w-64 mx-auto shadow-md border mt-4"> 
+      Hello world!
+    </h1>
+    <button>Kebede</button>
+    </>
+  )
+}
+
+export default App
