1f760dfbaa0a04c60f98a586d94d936683f55867
Nathaniel81 <nathanield356@gmail.com>
2023-12-13 16:47:59 +0300

    nc

--- rp6/vite-project/src/App.jsx
+++ rp6/vite-project/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   const [tasks, setTasks] = useState([]);
-  // console.log(tasks)
+  const [taskInput, setTaskInput] = useState("")
 
   useEffect(() => {
     const tasksData = async() => {
@@ -27,9 +27,18 @@ function App() {
     setTasks(updatedTasks);
   }
 
-  function handleSubmit(e) {
+   const handleSubmit = async(e) => {
     e.preventDefault()
-    console.log('Submit')
+      const res = fetch('http://127.0.0.1:8000/api/task-create', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({title: taskInput}),
+      })
+      const data = await res.json();
+      setTasks([...tasks, data]);
+      setTaskInput('');
   }
 
   return (
@@ -40,10 +49,18 @@ function App() {
             <form id="form" onSubmit={(e) => handleSubmit(e)}>
               <div className="flex-wrapper align-items-center p-5">
                   <div style={{flex: 6}}>
-                      <input className="form-control" id="title" type="text" name="title" placeholder="Add task.." />
+                      <input 
+                      className="form-control" 
+                      id="title" 
+                      value={taskInput} 
+                      type="text" 
+                      name="title" 
+                      placeholder="Add task.." 
+                      onChange={(e) => setTaskInput(e.target.value)}
+                      />
                    </div>
                    <div style={{flex: 1}}>
-                      <input id="submit" className="btn btn-warning" type="submit" name="Add" />
+                      <button id="submit" className="btn btn-warning" type="submit">Submit</button>
                     </div>
               </div>
             </form>
