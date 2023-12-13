dcec56bb2d9d99302f98ecc9ac79be0bfbd13553
Nathaniel81 <nathanield356@gmail.com>
2023-12-13 11:39:26 +0300

    nc

--- rp6/todo_app/todo/urls.py
+++ rp6/todo_app/todo/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-	path('', views.apiOverview, name='overview')
+	path('', views.apiOverview, name='overview'),
+	path('task-list', views.taskList, name='lists'),
+	path('task-detail/<str:pk>', views.taskDetail, name='detail')
 ]
--- rp6/todo_app/todo/views.py
+++ rp6/todo_app/todo/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 # from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerilaizer
 
 @api_view(['GET'])
 def apiOverview(request):
@@ -14,3 +16,15 @@ def apiOverview(request):
 		}
 
 	return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerilaizer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    serializer = TaskSerilaizer(task, many=False)
+    return Response(serializer.data)
