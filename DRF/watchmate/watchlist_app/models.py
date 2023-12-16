3b248c6dd47ac778cf76e7124930fb52f285e3a1
Nathaniel81 <nathanield356@gmail.com>
2023-12-16 20:12:33 +0300

    nc

--- DRF/watchmate/watchlist_app/models.py
+++ DRF/watchmate/watchlist_app/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class Movie(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=200)
-    active = models.booleanField(efault=False)
+    active = models.BooleanField(default=False)
     def __str__(self):
         return self.name
