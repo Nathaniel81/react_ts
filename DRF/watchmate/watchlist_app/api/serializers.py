3521d5ad0fce55cd070d7b8059c9a3e174461ce2
Nathaniel81 <nathanield356@gmail.com>
2023-12-18 16:28:40 +0300

    nc

--- DRF/watchmate/watchlist_app/api/serializers.py
+++ DRF/watchmate/watchlist_app/api/serializers.py
@@ -4,24 +4,28 @@ from watchlist_app.models import Movie
 def nameValidator(value):
     if len(value) < 2:
         raise serializers.ValidationError('Name is too short')
-class MovieSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    name = serializers.CharField(validators=[nameValidator])
-    description = serializers.CharField()
-    active = serializers.BooleanField()
-    def create(self, validated_data):
-        return Movie.objects.create(**validated_data)
-    def update(self, instance, validated_data):
-        instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.active = validated_data.get('active', instance.active)
-        instance.save()
-        return instance
-    # def validate_name(self, value):
-    #     if len(value) < 2:
-    #         raise serializers.ValidationError('Name is too short')
-    #     return value
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = '__all__'
+
     def validate(self, data):
         if data['name'] == data['description']:
-            raise serializers.ValidationError('')
+            raise serializers.ValidationError('Title & description should different')
         return data
+    def validate_name(self, value):
+        if len(value) < 2:
+            raise serializers.ValidationError('Name is too short')
+        return value
+    # id = serializers.IntegerField(read_only=True)
+    # name = serializers.CharField(validators=[nameValidator])
+    # description = serializers.CharField()
+    # active = serializers.BooleanField()
+    # def create(self, validated_data):
+    #     return Movie.objects.create(**validated_data)
+    # def update(self, instance, validated_data):
+    #     instance.name = validated_data.get('name', instance.name)
+    #     instance.description = validated_data.get('description', instance.description)
+    #     instance.active = validated_data.get('active', instance.active)
+    #     instance.save()
+    #     return instance
