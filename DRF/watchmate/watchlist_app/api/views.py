5597e91087a4d3c4566001f2d6d66fcb28adde14
Nathaniel81 <nathanield356@gmail.com>
2023-12-18 11:17:55 +0300

    nc

--- DRF/watchmate/watchlist_app/api/views.py
+++ DRF/watchmate/watchlist_app/api/views.py
@@ -2,6 +2,7 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from watchlist_app.models import Movie
 from .serializers import MovieSerializer
+from rest_framework import status
 
 @api_view(['GET', 'POST'])
 def movie_list(request):
@@ -18,21 +19,21 @@ def movie_list(request):
             return Response(serializer.errors)
 @api_view(['GET', 'PUT', 'DELETE'])
 def movie_detail(request, pk):
-    if request.method == 'GET':
+    try:
         movie = Movie.objects.get(pk=pk)
+    except Movie.DoesNotExist:
+        return Response({"Error': 'Movie doesn't exist"}, status=status.HTTP_404_NOT_FOUND)
+    if request.method == 'GET':
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
     if request.method == 'PUT':
-        movie = Movie.objects.get(pk=pk)
         serializer = MovieSerializer(instance=movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.error)
+            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
     if request.method == 'DELETE':
-        movie = Movie.objects.get(pk=pk)
         if movie:
             movie.delete()
-            return Response('Deleted Successfully')
-        
+            return Response(status=status.HTTP_204_NO_CONTENT)
