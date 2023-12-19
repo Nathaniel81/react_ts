3ac3c096d2067774db6d45cd054f824b1e7cfb43
Nathaniel81 <nathanield356@gmail.com>
2023-12-19 15:00:38 +0300

    nc

--- DRF/watchmate/watchlist_app/models.py
+++ DRF/watchmate/watchlist_app/models.py
@@ -6,11 +6,12 @@ class StreamPlatform(models.Model):
     website = models.URLField(max_length=100)
     def __str__(self):
         return self.name
-    
+
 class WatchList(models.Model):
     title = models.CharField(max_length=50)
     storyline = models.CharField(max_length=200)
     active = models.BooleanField(default=False)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name='watchlist')
     created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
-        return self.name
+        return self.title
