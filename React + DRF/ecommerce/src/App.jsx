a846447edc559886951a3562b04487e601fc35d6
Nathaniel81 <nathanield356@gmail.com>
2023-12-29 15:20:32 +0300

    nc

--- React + DRF/ecommerce/src/App.jsx
+++ React + DRF/ecommerce/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header />
+      <h2>Welcome</h2>
+      <Footer />
     </>
   )
 }
