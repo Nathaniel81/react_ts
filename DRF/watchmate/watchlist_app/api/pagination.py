f04f601047f2467204d083d4800ff75b93c1e357
Nathaniel81 <nathanield356@gmail.com>
2024-02-15 14:35:44 +0300

    nc

--- DRF/watchmate/watchlist_app/api/pagination.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination, CursorPagination
-
-class WatchListPagination(PageNumberPagination):
-    page_size = 2
-class ReviewListCPagination(CursorPagination):
-    page_size = 2
